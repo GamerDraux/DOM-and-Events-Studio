e67e25d8a9ee7e79f0532cb060881e6abc7c07e8
gamerDraux <61572578+GamerDraux@users.noreply.github.com>
2020-03-07 13:54:18 -0600

    added varInit function to pull id's to variables in script.js

--- scripts.js
+++ scripts.js
@@ -1,2 +1,22 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+
+let takeOff=null;
+let flightStatus=null;
+
+function varInit(){
+    takeOff = document.getElementById("takeoff");
+    flightStatus = document.getElementById("flightStatus");
+    console.log (takeOff);
+    console.log (flightStatus);
+    console.log ('window loaded');
+}
+
+
+
+takeOff.addEventListener('click', function(){
+    flightStatus.innerHTML="Shuttle in flight";
+});
+
+
+
