9a8c021a7712dddd59c795c7011702aa552f1d02
gamerDraux <61572578+GamerDraux@users.noreply.github.com>
2020-03-07 17:08:40 -0600

    added functions for takeoff, landing, and abort mission

--- scripts.js
+++ scripts.js
@@ -3,17 +3,45 @@
 
 let takeOff=null;
 let flightStatus=null;
+let shuttleBackground=null;
+let shuttleAltitudeObject = null;
+let land=null;
+let abort=null
 
 function varInit(){
     takeOff = document.getElementById("takeoff");
     flightStatus = document.getElementById("flightStatus");
-    console.log (takeOff);
-    console.log (flightStatus);
+    shuttleBackground = document.getElementById
+    ("shuttleBackground");
+    shuttleAltitudeObject = document.getElementById("spaceShuttleHeight");
+    shuttleAltitude=Number(shuttleAltitudeObject.innerHTML)
     console.log ('window loaded');
+    land = document.getElementById("landing");
+    abort= document.getElementById("missionAbort");
+};
+
+function launchShuttle(){
+            if (window.confirm("Confirm that the shuttle is ready for takeoff.")===true){
+                flightStatus.innerHTML="Shuttle in flight";
+                shuttleBackground.style.backgroundColor = "blue";
+                shuttleAltitude+=10000;
+                shuttleAltitudeObject.innerHTML=shuttleAltitude;
+            }
+};
+
+function landShuttle(){
+    alert("The shuttle is landing.  Landing gear engaged.");
+    flightStatus.innerHTML="The shuttle has landed.";
+    shuttleBackground.style.backgroundColor= "green";
+    shuttleAltitude=0;
+    shuttleAltitudeObject.innerHTML=shuttleAltitude;
 }
 
-takeOff.addEventListener('click', function(){
-            flightStatus.innerHTML="Shuttle in flight";
-            });
-
-
+function abortMission(){
+    if(window.confirm("Confirm that you want to abort the mission")===true){
+        flightStatus.innerHTML="Mission Aborted";
+        shuttleBackground.style.backgroundColor="green";
+        shuttleAltitude=0;
+        shuttleAltitudeObject.innerHTML=shuttleAltitude;
+    }
+}
